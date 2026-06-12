b6f8dcfb363e747e1cb4097ca0732afe2cdb959e
Diego Cintra <diegoffcintra@gmail.com>
2026-06-11 22:28:55 -0300

    first commit

---
+++ app/models/cateogry.ts
@@ -0,0 +1,4 @@
+import { CateogrySchema } from '#database/schema'
+
+export default class Cateogry extends CateogrySchema {
+}
---
+++ start/env.ts
@@ -0,0 +1,44 @@
+/*
+|--------------------------------------------------------------------------
+| Environment variables service
+|--------------------------------------------------------------------------
+|
+| The `Env.create` method creates an instance of the Env service. The
+| service validates the environment variables and also cast values
+| to JavaScript data types.
+|
+*/
+
+import { Env } from '@adonisjs/core/env'
+
+export default await Env.create(new URL('../', import.meta.url), {
+  // Node
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  PORT: Env.schema.number(),
+  HOST: Env.schema.string({ format: 'host' }),
+  LOG_LEVEL: Env.schema.string(),
+
+  // App
+  APP_KEY: Env.schema.secret(),
+  APP_URL: Env.schema.string({ format: 'url', tld: false }),
+
+  // Database
+  DB_CONNECTION: Env.schema.enum(['sqlite', 'pg'] as const),
+  DB_HOST: Env.schema.string.optional(),
+  DB_PORT: Env.schema.number.optional(),
+  DB_USER: Env.schema.string.optional(),
+  DB_PASSWORD: Env.schema.string.optional(),
+  DB_DATABASE: Env.schema.string.optional(),
+
+  // Redis
+  REDIS_HOST: Env.schema.string.optional(),
+  REDIS_PORT: Env.schema.number.optional(),
+
+  // Elasticsearch
+  ELASTICSEARCH_HOST: Env.schema.string.optional(),
+  ELASTICSEARCH_PORT: Env.schema.number.optional(),
+  ELASTICSEARCH_PROTOCOL: Env.schema.string.optional(),
+
+  // Session
+  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
+})
